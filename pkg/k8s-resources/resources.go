b84e7e7409d7789ad5fac7f9a924fd8fabfd9fc9
Igor <idyurev@gmail.com>
2020-03-08 11:24:23 +0300

    draft

---
+++ pkg/k8s-resources/resources.go
@@ -0,0 +1,57 @@
+package k8s_resources
+
+import (
+	"fmt"
+	"k8s.io/api/apps/v1beta1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func makeResources(cpuLimit, memLimit, cpuReq, memReq string) corev1.ResourceRequirements {
+	res := corev1.ResourceRequirements{
+		Limits: map[corev1.ResourceName]resource.Quantity{
+			corev1.ResourceLimitsCPU:    resource.MustParse(cpuLimit),
+			corev1.ResourceLimitsMemory: resource.MustParse(memLimit),
+		},
+		Requests: map[corev1.ResourceName]resource.Quantity{
+			corev1.ResourceRequestsCPU:    resource.MustParse(cpuReq),
+			corev1.ResourceRequestsMemory: resource.MustParse(memReq),
+		},
+	}
+	return res
+}
+
+func makeCommonLabels(appName, deploymentName string) map[string]string {
+	return map[string]string{
+		"app":        appName,
+		"deployment": deploymentName,
+	}
+}
+
+func commonServiceAccount(saName, namespace string) (corev1.ServiceAccount, error) {
+	if saName == "" {
+		return corev1.ServiceAccount{}, fmt.Errorf("Empty service account name!!!")
+	}
+	if namespace == "" {
+		return corev1.ServiceAccount{}, fmt.Errorf("Empty namespace name!!!")
+	}
+	sa := corev1.ServiceAccount{
+		TypeMeta: v1.TypeMeta{
+			Kind:       "ServiceAccount",
+			APIVersion: "v1",
+		},
+		ObjectMeta: v1.ObjectMeta{
+			Name:      saName,
+			Namespace: namespace,
+		},
+		Secrets:                      nil,
+		ImagePullSecrets:             nil,
+		AutomountServiceAccountToken: nil,
+	}
+	return sa, nil
+}
+
+func commonDeployment(name, image string, port int) (v1beta1.Deployment, error) {
+	return v1beta1.Deployment{}, nil
+}
